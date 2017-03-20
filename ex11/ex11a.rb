89d0baaa7a02fe5c665022ce63527259359d3919
itsalwaysbenny <taylor.ben89@gmail.com>
2017-03-20 20:35:48 +0000

    ex11a

---
+++ ex11/ex11a.rb
@@ -0,0 +1,10 @@
+print "Do you have a favourite restaurant? "
+restaurant = gets.chomp
+print "What kind or food do they serve? "
+food = gets.chomp
+print "Do you have a meal reccommendation? "
+meal = gets.chomp
+print "That sounds tasty, where is it? "
+place = gets.chomp
+
+puts "Sounds great I love #{food}, I'm going to try out #{meal} at #{restaurant} next time I'm in #{place}"
