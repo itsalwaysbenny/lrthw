b6446bfaf846064736a4d0a6d18345dbd20ae673
itsalwaysbenny <taylor.ben89@gmail.com>
2017-03-20 18:26:23 +0000

    ex9

---
+++ ex9/ex9.rb
@@ -0,0 +1,16 @@
+# Here's some strange stuff, remeber type it exactly.
+
+# backslash \ cancels the next character
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# %q{ is the same as using a single quotation mark }
+puts %q{
+    There's something going on here.
+    With the three double-quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+}
