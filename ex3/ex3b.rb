508100fe85a284427cfeedba91e7bc429daa3834
itsalwaysbenny <taylor.ben89@gmail.com>
2017-03-20 14:54:48 +0000

    ex3c

---
+++ ex3/ex3b.rb
@@ -0,0 +1,43 @@
+#+ plus addition 
+#- minus subtraction
+#/ slash division
+#* asterisk addititon
+#% percent finding remainder
+#< less-than
+#> greater-than
+#<= less-than-equal
+#>= greater-than-equal
+
+#Puts the string to terminal
+puts "I will now count my chickens:"
+
+#Does the maths in order of BIDMAS and puts the answer
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#Puts the string to terminal
+puts "Now I will count the eggs:"
+
+#Puts 7 becuase number is rounded up from 6.75
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Puts the string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Does the maths, answers false
+puts 3 + 2 < 5 - 7
+
+#Puts the string and answer to the sum
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+#Puts the string
+puts "Oh, that's why it's false."
+
+#Puts the string
+puts "How about some more."
+
+#Puts the string and answers with true/false
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ ex3/ex3c.rb
@@ -0,0 +1,6 @@
+#What is the difference in my commute time each day/week/month
+puts "It now takes me 75 minutes to commute to work each day"
+puts "It will take me 45 minutes to commute to work each day"
+puts "The dfference in a one way commute will be #{1.25 - 0.75}"
+puts "So everyday I will save #{(1.25 - 0.75)*2} hours"
+puts "That's #{((1.25 - 0.75)*2)*5} hours a week. Wow!!!"
